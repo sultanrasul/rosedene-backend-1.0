42bac694efa912ecc6c858592ca2272d2827fffa
Funky Panda <72228432+Funky-Panda@users.noreply.github.com>
2025-03-19 22:56:59 +0000

    added docker file to see if this works on google cloud run

--- main.py
+++ main.py
@@ -654,6 +654,5 @@ def get_reviews():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))  # Use the PORT variable or default to 8080
-    app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
